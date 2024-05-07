4b28f804990cd7b836954e728325496d9a3511b3
Karina Portuguez <karinaportuguezh@gmail.com>
2024-05-07 17:35:16 -0600

    Uploading notes

---
+++ app.ts
@@ -0,0 +1,7 @@
+
+console.log('Hola Mundo :D\n');  // console.log(); es un metrodo.
+
+let a = 10;
+
+console.log(`Este es un template literal que tiene una variable "let a" con el valor de: ${a} \n`);
+
