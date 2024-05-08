5cfad8606d6f54568d9579620622e86fb58123ea
Karina Portuguez <karinaportuguezh@gmail.com>
2024-05-07 18:43:27 -0600

    Functions

--- app.ts
+++ app.ts
@@ -5,3 +5,34 @@ let a = 10;
 
 console.log(`Este es un template literal que tiene una variable "let a" con el valor de: ${a} \n`);
 
+let numbersOfLines = 1;
+
+// FUNCIONES : Es una serie de instrucciones que se ejecutan de manera controlada
+
+function increaseLineNumber() {
+  //numbersOfLines = numbersOfLines + 3;
+  numbersOfLines += 3;
+}
+/* 
+console.log(`Linea #${numbersOfLines}`);
+
+numbersOfLines++;
+console.log(`Linea #${numbersOfLines}`);
+
+numbersOfLines += 1;
+console.log(`Linea #${numbersOfLines}`);
+
+numbersOfLines = numbersOfLines + 1;
+console.log(`Linea #${numbersOfLines}`); 
+*/
+
+console.log(`Linea #${numbersOfLines}`);
+
+increaseLineNumber()
+console.log(`Linea #${numbersOfLines}`);
+
+increaseLineNumber()
+console.log(`Linea #${numbersOfLines}`);
+
+increaseLineNumber()
+console.log(`Linea #${numbersOfLines}`); 
