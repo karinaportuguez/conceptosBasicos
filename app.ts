cc439047793c36f37cd810e0ad7d42a8eeef0ca8
Karina Portuguez <karinaportuguezh@gmail.com>
2024-05-12 17:30:24 -0600

    Introduccion section 4

--- app.ts
+++ app.ts
@@ -1,5 +1,19 @@
-console.log(' \nStarting the program...\n\n  |------------------------------------|\n  |-----------SECTION 3----------------|\n  |------------- LOOPS ----------------|\n  |____________________________________|\n\n');
+console.log(' \nStarting the program...\n\n  |------------------------------------|\n  |-----------SECTION 4----------------|\n  |----------- FUNCTIONS --------------|\n  |____________________________________|\n\n');
 
+/* 
+In this section we will continue introducing new concepts that are important in every program, such as:
 
+Arrays
+
+Functions
+
+Imports
+
+Exports
+
+Code reuse
+
+What we will see here forms the basis of the DRY (Don't repeat yourself) principle.
+ */
 
 console.log(`\nEnd of program...\n`);
