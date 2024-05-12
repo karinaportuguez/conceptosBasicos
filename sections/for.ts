87870bd06389d8c7b02acc3e77e7fe05e9f1d756
Karina Portuguez <karinaportuguezh@gmail.com>
2024-05-12 12:52:40 -0600

    add secction for

--- sections/for.ts
+++ sections/for.ts
@@ -1,4 +1,19 @@
 console.log(' \nStarting the program...\n\n  |------------------------------------|\n  |-----------SECTION 3----------------|\n  |------------- LOOPS ----------------|\n  |____________________________________|\n\n');
+// FOR
+export let limite = 40;
+for (let i = 1; i <= limite; i++) {
 
+  if (i === 5) {
+    continue; // Jump
+  }
+
+  console.log(`let i: ${i}`);
+
+
+  if (i === 5) {
+    break;
+  }
+
+}
 
 console.log(`\nEnd of program...\n`);
