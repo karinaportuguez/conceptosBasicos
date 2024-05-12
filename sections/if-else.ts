1bfcdac05ac97372dde14af4265cc60f9b679c2c
Karina Portuguez <karinaportuguezh@gmail.com>
2024-05-12 11:43:25 -0600

    if else secction add

---
+++ sections/if-else.ts
@@ -0,0 +1,32 @@
+console.log(' \n  |------------------------------------|\n  |-----------SECTION 3----------------|\n  |----------- IF ELSE ------------|\n  |____________________________________|\n\n');
+
+// if
+/* 
+let isTired = !true;
+
+if (isTired) {  // (isTired === true)
+  console.log(` Drink cup of coffee☕, you are low energy 🪫  \n`)
+} else {
+  console.log(`🔋 You are full of energy 😄, drink a glass of water 🫗 \n`);
+}
+ */
+
+/* 
+if (!isTired) {
+  console.log(`🔋 You are full of energy 😄 \n`);
+}
+ */
+
+export let grade: number = 100;
+
+if (grade >= 60) {
+  console.log(`The students pass the class ✅\n`);
+} else if (grade > 50) {
+  console.log(`Please study more 🏫🧐\n`);
+} else {
+  console.log(`The students failed class ❌\n `);
+}
+
+console.log(`\n\nFin del programa...\n`);
+
+//If else anidado no se debe hacer
